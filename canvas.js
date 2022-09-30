b1ae2860e8f4eacb824dd99945d3ee283b735223
Jahidul Alam <mjahidulalam@gmail.com>
2022-09-30 00:21:07 -0600

    updated farthest insertion

--- canvas.js
+++ canvas.js
@@ -39,6 +39,23 @@ function plotPoints(points, length, base_color = "red", base_size = 7, node_colo
     }
 }
 
+function drawCycles(solution, line_color = "white", line_width = 3){
+    let node = solution.head
+        for (var index = 0; index < solution.length; index++) {
+            if (index == 0) {
+                c.clearRect(0, 0, canvas.width, canvas.height);
+                c.putImageData(imageData, 0, 0);
+                c.beginPath();
+                c.strokeStyle = line_color;
+                c.lineWidth = line_width;
+                c.moveTo(node.coord[0], node.coord[1]);
+            }
+            c.lineTo(node.coord[0], node.coord[1]);
+            node = node.next
+        }
+        c.stroke(); 
+}
+
 function distance(point1, point2){
     return Math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2);
 }
@@ -149,7 +166,7 @@ class LinkedList {
     }
 }
 
-function NearestNeighbors(points, current_node = 0) {
+function NearestNeighbors(points, speed, current_node = 0) {
     if (current_node == 0) {        
         var current_node = points[0]
         delete points[0];
@@ -173,11 +190,11 @@ function NearestNeighbors(points, current_node = 0) {
         c.stroke();
         current_node = points[closest_node];
         delete points[closest_node];
-        setTimeout(function(){NearestNeighbors(points, current_node = current_node)}, 200);
+        setTimeout(function(){NearestNeighbors(points, speed, current_node = current_node)}, speed);
     }
 }
 
-function NearestInsertion(points, solution = null) {
+function NearestInsertion(points, speed, tourDist = 0, solution = null) {
     if (solution == null) {
         solution = new LinkedList(0, points[0])
         solution.append(0, points[0])
@@ -188,8 +205,59 @@ function NearestInsertion(points, solution = null) {
     let minDist = Infinity;
     let nextInsert = null;
     for (var index = 0; index < solution.length-1; index++) {
+        nextNode = currentNode.next
+        let dist_ab = adjList[[currentNode.value, nextNode.value]]
+
         for (var i in points) {
-            let dist = 0;
+            let dist_aib = 0;
+            for (node of [currentNode, nextNode]) {
+                let dist = null
+                if (!adjList[[node.value, i]]) {
+                    dist = distance(node.coord, points[parseInt(i)])
+                    adjList[[node.value, i]] = dist
+                    adjList[[i, node.value]] = dist
+                } else {
+                    dist = adjList[[node.value, i]]
+                }
+
+                dist_aib = dist_aib + dist
+            }
+
+            if (tourDist - dist_ab + dist_aib < minDist) {
+                minDist = tourDist - dist_ab + dist_aib
+                nextInsert = i;
+                insertPosition = index+1;
+            }
+        }
+        currentNode = currentNode.next
+    }
+
+    solution.insert(nextInsert, points[nextInsert], insertPosition)
+    tourDist = minDist
+
+    c.strokeStyle = 'white';
+    if (Object.keys(points).length < 1) {
+        return;
+    } else {
+        drawCycles(solution)
+        delete points[nextInsert];
+        setTimeout(function(){NearestInsertion(points, speed, tourDist = tourDist, solution = solution)}, speed);
+    }
+}
+
+function FarthestInsertion(points, speed, tourDist = 0, solution = null) {
+    if (solution == null) {
+        solution = new LinkedList(0, points[0])
+        solution.append(0, points[0])
+        delete points[0];
+    }
+
+    let currentNode = solution.head;
+    let maxDist = 0;
+    let nextInsert = null;
+    for (var index = 0; index < solution.length-1; index++) {
+        for (var i in points) {
+            let dist = null;
             if (!adjList[[currentNode.value, i]]) {
                 dist = distance(currentNode.coord, points[parseInt(i)])
                 adjList[[currentNode.value, i]] = dist
@@ -198,61 +266,51 @@ function NearestInsertion(points, solution = null) {
                 dist = adjList[[currentNode.value, i]]
             }
 
-            if (dist < minDist) {
-                minDist = dist;
+            if (dist > maxDist) {
+                maxDist = dist;
                 nextInsert = i;
-                insertPosition = index;
             }
         }
         currentNode = currentNode.next
     }
 
-    if (solution.length <= 3) {
-        solution.insert(nextInsert, points[nextInsert], 1)
-    } else {
-        if (insertPosition == 0) {
-            var node_a = solution.tail.value
-            var node_b = solution.head.value
-            var node_c = solution.head.next.value
-        } else {
-            let positionNodes = solution.lookup(insertPosition-1)
-            var node_a = positionNodes.value
-            var node_b = positionNodes.next.value
-            var node_c = positionNodes.next.next.value
+    currentNode = solution.head;
+    let minTourDist = Infinity;
+    for (var index = 0; index < solution.length-1; index++) {
+        nextNode = currentNode.next
+        let dist_ab = adjList[[currentNode.value, nextNode.value]]
+        let dist_aib = 0;
+
+        for (node of [currentNode, nextNode]) {
+            let dist = null
+            if (!adjList[[node.value, nextInsert]]) {
+                dist = distance(node.coord, points[parseInt(nextInsert)])
+                adjList[[node.value, nextInsert]] = dist
+                adjList[[nextInsert, node.value]] = dist
+            } else {
+                dist = adjList[[node.value, nextInsert]]
+            }
+
+            dist_aib = dist_aib + dist
         }
 
-        let dist_adb = adjList[[node_a, nextInsert]] + adjList[[nextInsert, node_b]] - adjList[[node_a, node_b]]
-        let dist_bdc = adjList[[node_b, nextInsert]] + adjList[[nextInsert, node_c]] - adjList[[node_b, node_c]]
-
-        if (dist_adb < dist_bdc) {
-            solution.insert(nextInsert, points[nextInsert], insertPosition)
-        } else {
-            solution.insert(nextInsert, points[nextInsert], insertPosition+1)
+        if (tourDist - dist_ab + dist_aib < minTourDist) {
+            minTourDist = tourDist - dist_ab + dist_aib
+            insertPosition = index+1;
         }
+        currentNode = currentNode.next
     }
 
+    solution.insert(nextInsert, points[nextInsert], insertPosition)
+    tourDist = minTourDist
+
     c.strokeStyle = 'white';
-    if (Object.keys(points).length < 1) {   
+    if (Object.keys(points).length < 1) {
         return;
     } else {
-        let node = solution.head
-        for (var index = 0; index < solution.length; index++) {
-            // alert(PointsLength)
-            if (index == 0) {
-                c.clearRect(0, 0, canvas.width, canvas.height);
-                c.putImageData(imageData, 0, 0);
-                c.beginPath();
-                c.strokeStyle = "white";
-                c.lineWidth = 3;
-                origin_coords = points[0]
-                c.moveTo(node.coord[0], node.coord[1]);
-            }
-            c.lineTo(node.coord[0], node.coord[1]);
-            node = node.next
-        }
-        c.stroke(); 
+        drawCycles(solution)
         delete points[nextInsert];
-        setTimeout(function(){NearestInsertion(points, solution = solution)}, 200);
+        setTimeout(function(){FarthestInsertion(points, speed, tourDist = tourDist, solution = solution)}, speed);
     }
 }
 
@@ -260,11 +318,26 @@ function run_animation() {
     c.clearRect(0, 0, canvas.width, canvas.height);
     const PointsLength = document.getElementById('node_input').valueAsNumber;
     const algo = document.querySelector('input[name="algo-input"]:checked').value;
+    const speed_input = document.querySelector('input[name="speed-input"]').value;
 
     let points = createRandomPoints(PointsLength, canvas.width, canvas.height);
     plotPoints(points, PointsLength)
 
     imageData = c.getImageData(0,0,canvas.width,canvas.height);
+
+    if (PointsLength < 50) {
+        speed_value = 200;
+    } else if (PointsLength >= 50 && PointsLength < 100) {
+        speed_value = 100;
+    } else if (PointsLength >= 100 && PointsLength < 300) {
+        speed_value = 50;
+    } else if (PointsLength >= 300 && PointsLength < 500) {
+        speed_value = 20;
+    } else {
+        speed_value = 10;
+    }
+
+    alert(speed_value)
 
     c.beginPath();
     c.strokeStyle = "white";
@@ -273,10 +346,17 @@ function run_animation() {
     c.moveTo(points[0][0], points[0][1]);
 
     if (algo == "NN") {
-        NearestNeighbors(points)
+        NearestNeighbors(points, speed = speed_value)
+    } else if (algo == "NI") {
+        adjList = {"0,0": 0}
+        NearestInsertion(points, speed = speed_value)
+        return;
+    } else if (algo == "FI") {
+        adjList = {"0,0": 0}
+        FarthestInsertion(points, speed = speed_value)
+        return;
     } else {
-        adjList = {}
-        NearestInsertion(points)
+        alert("Not yet implemented")
         return;
     }
 }
